b6a7921c8eb45b02095672f29c8ddba58dd89c71
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-06-12 09:34:49 +0200

    feat: implement select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,55 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+        <SelectComponent value={value} aria-label={label} onChange={onChange}>
+        {children}
+      </SelectComponent>
+      <StyledIcon id="chevron-down" size={24}/>
+    </Wrapper>
   );
 };
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  top: 19px;
+  bottom: 18px;
+  margin-top: auto;
+  margin-bottom: auto;
+  right: 8px;
+  color: ${COLORS.gray700};
+  pointer-events: none;
+`;
+
+const SelectComponent = styled.select`
+  height: 43px;
+  appearance: none;
+  text-indent: 1px;
+  text-overflow: '';
+  padding: 12px 52px 12px 16px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-family: "Roboto", "Arial";
+  border-radius: 8px;
+  border: none;
+  
+  &:focus {
+    outline: default;
+    border: 2px solid default;
+  }
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+
+  &:hover ${SelectComponent} {
+    color: black;
+  }
+
+  &:hover ${StyledIcon} {
+    color: black;
+  }
+`;
 
 export default Select;
